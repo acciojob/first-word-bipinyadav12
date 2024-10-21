adc1e9b1f59cfbc6419031854d3b5220b7f52ad3
bipinyadav12 <159446360+bipinyadav12@users.noreply.github.com>
2024-10-21 22:26:17 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 function firstWord(s) {
-  // your code here
+ const arr= s.split(" ");
+	return arr[0];
 }
 
 // Do not change the code below
 
 const s = prompt("Enter String:");
 alert(firstWord(s));
+
